9bf5fcfb4f42b33a46c76ae5e17a8d7c7d2ead05
Mani <mani.rus3@gmail.com>
2026-01-01 10:55:12 +0100

    Page support

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Layout from './components/Layout';
 import Pricing from './pages/Pricing';
 import Success from './pages/Success';
 import Usage from './pages/Usage';
-import Support from './components/Support';
+import Support from './pages/Support';
 import './index.css';
 
 function App() {
